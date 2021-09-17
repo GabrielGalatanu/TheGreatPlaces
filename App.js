ba2a5e6afa118698a2a789ca9ef28c7c604612bc
Gabi Galatanu <mihai.gabriel.galatanu@gmail.com>
2021-09-17 09:45:56 +0300

    First Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text> ok test </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
